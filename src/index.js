9af921d79c241a574f304131d06de11ff5e35b24
ytserilo <acemirskija@gmail.com>
2023-09-15 15:43:18 +0300

    Fix router basename.

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ import "./index.css";
 import "highlight.js/styles/github.css";
 
 const router = createBrowserRouter(createRoutesFromElements(routes), {
-  basename: "/",
+  basename: process.env.PUBLIC_URL,
 });
 
 const Index = ({ children }) => {
